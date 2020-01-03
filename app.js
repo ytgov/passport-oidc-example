426ef6406b9b1b6605d070012b45dd5229fe9eb7
Ryan Agar <ryanjagar@gmail.com>
2020-01-03 09:53:24 -0800

    initial commit w yg skinning

--- app.js
+++ app.js
@@ -10,6 +10,28 @@ var OidcStrategy = require('passport-openidconnect').Strategy
 var indexRouter = require('./routes')
 var usersRouter = require('./routes/users')
 
+var vivvo= {
+    issuer: 'https://yukon.vivvocloud.com/',
+    authorizationURL: 'https://yukon.vivvocloud.com/oauth/v2/authorize',
+    tokenURL: 'https://yukon.vivvocloud.com/oauth/v2/token',
+    userInfoURL: 'https://yukon.vivvocloud.com/oauth/v2/userinfo',
+    clientID: '002e38db-2a03-4dc6-aedb-8732bb9e4060',
+    clientSecret: 'cc3fe360-86e1-4fba-a74c-d78c5450f50d',
+    callbackURL: 'http://localhost:3000/authorization-code/callback',
+    scope: 'openid profile email phone address'
+  }
+
+var auth0 = {
+    issuer: 'https://dev-0tc6bn14.eu.auth0.com/',
+    authorizationURL: 'https://dev-0tc6bn14.eu.auth0.com/authorize',
+    tokenURL: 'https://dev-0tc6bn14.eu.auth0.com/oauth/token',
+    userInfoURL: 'https://dev-0tc6bn14.eu.auth0.com/userinfo',
+    clientID: 'qce9HfoO2Gnrz1oAHERhANcnDfJngAuJ',
+    clientSecret: 'TTdc5Wryh-9zwDpxwoQRNr1cMELEz6G9srIW46fceQe5MvwoVo2Y5Hd468ZnObRj',
+    callbackURL: 'http://localhost:3000/authorization-code/callback',
+    scope: 'openid profile email phone address'
+}
+
 var app = express()
 
 // view engine setup
@@ -32,18 +54,23 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 // set up passport
-passport.use('oidc', new OidcStrategy({
+/*passport.use('oidc', new OidcStrategy({
   issuer: 'https://yukon.vivvocloud.com',
   authorizationURL: 'https://yukon.vivvocloud.com/oauth/v2/authorize',
   tokenURL: 'https://yukon.vivvocloud.com/oauth/v2/token',
   userInfoURL: 'https://yukon.vivvocloud.com/oauth/v2/userinfo',
-  clientID: '',
-  clientSecret: '',
+  clientID: '002e38db-2a03-4dc6-aedb-8732bb9e4060',
+  clientSecret: 'cc3fe360-86e1-4fba-a74c-d78c5450f50d',
   callbackURL: 'http://localhost:3000/authorization-code/callback',
   scope: 'openid profile email phone address'
 }, (issuer, sub, profile, accessToken, refreshToken, done) => {
   return done(null, profile)
 }))
+*/
+passport.use('oidc', new OidcStrategy(vivvo, 
+    (issuer, sub, profile, accessToken, refreshToken, done) => {
+    return done(null, profile)
+  }))
 
 passport.serializeUser((user, next) => {
   next(null, user)
@@ -74,7 +101,7 @@ function ensureLoggedIn (req, res, next) {
 }
 
 app.use('/profile', ensureLoggedIn, (req, res) => {
-  res.render('profile', { title: 'Express', user: req.user })
+  res.render('profile', { title: 'Energy Rebate', user: req.user })
 })
 
 app.get('/logout', (req, res) => {
